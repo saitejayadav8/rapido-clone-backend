aef3e3b28330d176c61c1e13eaa777c56d79e3f9
Sai Teja <tejayadav130@gmail.com>
2026-05-28 14:55:45 +0530

    Completed driver profile availability and online status APIs

--- driver-service/src/main/java/com/rapido/driver_service/repository/DriverRepository.java
+++ driver-service/src/main/java/com/rapido/driver_service/repository/DriverRepository.java
@@ -1,16 +1,19 @@
 package com.rapido.driver_service.repository;
 
 import com.rapido.driver_service.entity.Driver;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
 
-    Optional<Driver> findByEmail(String email);
-
     boolean existsByEmail(String email);
+
+    Optional<Driver> findByEmail(String email);
 
     List<Driver> findByAvailableTrueAndOnlineTrue();
 }
